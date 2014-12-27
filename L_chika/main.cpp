351a7b561793472cd07a7fa5594450939861b5d7
hirakuni45 <hira@rvf-rc45.net>
2014-12-28 07:47:52 +0900

    high speed internal clock enables

--- L_chika/main.cpp
+++ L_chika/main.cpp
@@ -2,15 +2,35 @@
 #include "clock.hpp"
 #include "port.hpp"
 
+static void wait_(uint16_t n)
+{
+	while(n > 0) {
+		asm("nop");
+		--n;
+	}
+}
+
+
 int main(int argc, char *ragv[])
 {
 	using namespace device;
 
-	PD1.B0 = 1;
+// クロック関係レジスタ・プロテクト解除
+	PRCR.PRC0 = 1;
 
+// 高速オンチップオシレーターへ切り替え
+	OCOCR.HOCOE = 1;
+	wait_(1000);
+	SCKCR.HSCKSEL = 1;
+	CKSTPR.SCKSEL = 1;
+
+// L チカ・メイン
+	PD1.B0 = 1;
 	while(1) {
-		for(int i = 0; i < 1000; ++i) P1.B0 = 0;
-		for(int i = 0; i < 1000; ++i) P1.B0 = 1;
+		P1.B0 = 0;
+		for(uint32_t i = 0; i < 300000; ++i) { asm("nop"); }
+		P1.B0 = 1;
+		for(uint32_t i = 0; i < 300000; ++i) { asm("nop"); }
 	}
 
 }
