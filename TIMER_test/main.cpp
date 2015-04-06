48ef68be69e71cf26c4f9224661765a0f7245b82
hirakuni45 <hira@rvf-rc45.net>
2015-04-06 22:22:59 +0900

    タイマーテストプロジェクト

---
+++ TIMER_test/main.cpp
@@ -0,0 +1,99 @@
+//=====================================================================//
+/*!	@file
+	@brief	R8C メイン
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "system.hpp"
+#include "clock.hpp"
+#include "port.hpp"
+#include "intr.hpp"
+#include "timer_rj.hpp"
+#include "timer_rb.hpp"
+#include "timer_rc.hpp"
+
+static void wait_(uint16_t n)
+{
+	while(n > 0) {
+		asm("nop");
+		--n;
+	}
+}
+
+extern "C" {
+	void null_task_(void);
+	void brk_inst_(void);
+}
+
+extern "C" {
+	const void* variable_vectors_[] __attribute__ ((section (".vvec"))) = {
+		(void*)brk_inst_,   nullptr,	// (0)
+		(void*)null_task_,  nullptr,	// (1) flash_ready
+		(void*)null_task_,  nullptr,	// (2)
+		(void*)null_task_,  nullptr,	// (3)
+
+		(void*)null_task_,  nullptr,	// (4) コンパレーターB1
+		(void*)null_task_,  nullptr,	// (5) コンパレーターB3
+		(void*)null_task_,  nullptr,	// (6)
+		(void*)null_task_,  nullptr,	// (7) タイマＲＣ
+
+		(void*)null_task_,  nullptr,	// (8)
+		(void*)null_task_,  nullptr,	// (9)
+		(void*)null_task_,  nullptr,	// (10)
+		(void*)null_task_,  nullptr,	// (11)
+
+		(void*)null_task_,  nullptr,	// (12)
+		(void*)null_task_,  nullptr,	// (13) キー入力
+		(void*)null_task_,  nullptr,	// (14) A/D 変換
+		(void*)null_task_,  nullptr,	// (15)
+
+		(void*)null_task_,  nullptr,	// (16)
+		(void*)null_task_,  nullptr,   // (17) UART0 送信
+		(void*)null_task_,  nullptr,   // (18) UART0 受信
+		(void*)null_task_,  nullptr,	// (19)
+
+		(void*)null_task_,  nullptr,	// (20)
+		(void*)null_task_,  nullptr,	// (21) /INT2
+		(void*)null_task_,  nullptr,	// (22) タイマＲＪ２
+		(void*)null_task_,  nullptr,	// (23) 周期タイマ
+
+		(void*)null_task_,  nullptr,	// (24) タイマＲＢ２
+		(void*)null_task_,  nullptr,	// (25) /INT1
+		(void*)null_task_,  nullptr,	// (26) /INT3
+		(void*)null_task_,  nullptr,	// (27)
+
+		(void*)null_task_,  nullptr,	// (28)
+		(void*)null_task_,  nullptr,	// (29) /INT0
+		(void*)null_task_,  nullptr,	// (30)
+		(void*)null_task_,  nullptr,	// (31)
+	};
+}
+
+int main(int argc, char *ragv[])
+{
+	using namespace device;
+
+// クロック関係レジスタ・プロテクト解除
+	PRCR.PRC0 = 1;
+
+// 高速オンチップオシレーターへ切り替え(20MHz)
+// ※ F_CLK を設定する事（Makefile内）
+	OCOCR.HOCOE = 1;
+	wait_(1000);
+	SCKCR.HSCKSEL = 1;
+	CKSTPR.SCKSEL = 1;
+
+	// モジュールスタンバイ制御
+	MSTCR.MSTUART = 0;
+
+	// L チカ・メイン
+	PD1.B0 = 1;
+	uint8_t v = 0;
+	while(1) {
+		P1.B0 = v;
+		for(uint32_t i = 0; i < 50000; ++i) {
+
+		}
+		v ^= 1;
+	}
+}
