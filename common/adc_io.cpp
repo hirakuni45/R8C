1b1f679a5d29723e7b731957f25d3a3a33e065fa
hirakuni45 <hira@rvf-rc45.net>
2015-04-09 02:29:48 +0900

    A/D 変換制御クラス、追加

---
+++ common/adc_io.cpp
@@ -0,0 +1,15 @@
+//=====================================================================//
+/*!	@file
+	@brief	R8C グループ・A/D 制御 @n
+			Copyright 2015 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/adc_io.hpp"
+
+namespace device {
+
+
+
+}
+
