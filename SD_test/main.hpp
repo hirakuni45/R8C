c41b9bcd724a6acea33cd740b32c4d1ec30c2150
hirakuni45 <hira@rvf-rc45.net>
2015-04-28 00:33:55 +0900

    SD カードテスト新規

---
+++ SD_test/main.hpp
@@ -0,0 +1,16 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	R8C メイン・ヘッダー
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/uart_io.hpp"
+#include "common/trb_io.hpp"
+#include "common/adc_io.hpp"
+
+typedef device::uart_io<device::UART0, 16, 16> uart0;
+
+typedef device::trb_io timer_b;
+
+typedef device::adc_io adc;
