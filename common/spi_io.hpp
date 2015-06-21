6fb7a2210e12e42417995c18683b7f477b9d92f7
hirakuni45 <hira@rvf-rc45.net>
2015-06-22 01:40:52 +0900

    add SPI driver class

---
+++ common/spi_io.hpp
@@ -0,0 +1,69 @@
+#pragma once
+//=====================================================================//
+/*!	@file
+	@brief	SPI テンプレートクラス (20MHz system clock) @n
+			Copyright 2015 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include <cstdint>
+#include "common/delay.hpp"
+
+namespace device {
+
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	/*!
+		@brief  SPI テンプレートクラス @n
+				PORT ポート指定クラス： @n
+				class port { @n
+				public: @n
+					void scl_out(bool val) const { } @n
+					void sda_out(bool val) const { } @n
+					bool sda_inp() const { return b; } @n
+				};
+		@param[in]	PORT	ポート定義クラス
+	*/
+	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
+	template <class PORT>
+	class spi_io {
+		PORT		port_;
+
+	public:
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  コンストラクター
+		*/
+		//-----------------------------------------------------------------//
+		spi_io() { }
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  初期化
+		*/
+		//-----------------------------------------------------------------//
+		void init() const {
+			port_.init();
+			port_.scl_out(1);
+		}
+
+
+		//-----------------------------------------------------------------//
+		/*!
+			@brief  ライト
+			@param[in]	d	データ
+		*/
+		//-----------------------------------------------------------------//
+		void write(uint8_t d) const {
+			for(uint8_t i = 0; i < 8; ++i) {
+				port_.scl_out(0);
+				port_.sda_out(d & 0x80);
+//				utils::delay::micro_second(clock_);
+				port_.scl_out(1);
+				d <<= 1;
+//				utils::delay::micro_second(clock_);
+			}
+		}
+
+	};
+}
