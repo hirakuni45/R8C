afacd974fce57008e8cc8cb50c5c3c5788b9c290
hirakuni45 <hira@rvf-rc45.net>
2015-07-07 03:16:26 +0900

    move from L_chika

---
+++ LED_test/main.cpp
@@ -0,0 +1,89 @@
+//=====================================================================//
+/*!	@file
+	@brief	R8C L チカ・メイン @n
+			LED は、Ｐ１、０に接続（吸い込み点灯）@n
+			※M120AN(20)
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "common/vect.h"
+#include "system.hpp"
+#include "clock.hpp"
+#include "port.hpp"
+
+static void wait_(uint16_t n)
+{
+	while(n > 0) {
+		asm("nop");
+		--n;
+	}
+}
+
+extern "C" {
+	const void* variable_vectors_[] __attribute__ ((section (".vvec"))) = {
+		(void*)brk_inst_,   nullptr,	// (0)
+		(void*)null_task_,  nullptr,	// (1) flash_ready
+		(void*)null_task_,  nullptr,	// (2)
+		(void*)null_task_,  nullptr,	// (3)
+
+		(void*)null_task_,  nullptr,	// (4) コンパレーターB1
+		(void*)null_task_,  nullptr,	// (5) コンパレーターB3
+		(void*)null_task_,  nullptr,	// (6)
+		(void*)null_task_,  nullptr,	// (7) タイマＲＣ
+
+		(void*)null_task_,  nullptr,	// (8)
+		(void*)null_task_,  nullptr,	// (9)
+		(void*)null_task_,  nullptr,	// (10)
+		(void*)null_task_,  nullptr,	// (11)
+
+		(void*)null_task_,  nullptr,	// (12)
+		(void*)null_task_,  nullptr,	// (13) キー入力
+		(void*)null_task_,  nullptr,	// (14) A/D 変換
+		(void*)null_task_,  nullptr,	// (15)
+
+		(void*)null_task_,  nullptr,	// (16)
+		(void*)null_task_,  nullptr,   // (17) UART0 送信
+		(void*)null_task_,  nullptr,   // (18) UART0 受信
+		(void*)null_task_,  nullptr,	// (19)
+
+		(void*)null_task_,  nullptr,	// (20)
+		(void*)null_task_,  nullptr,	// (21) /INT2
+		(void*)null_task_,  nullptr,	// (22) タイマＲＪ２
+		(void*)null_task_,  nullptr,	// (23) 周期タイマ
+
+		(void*)null_task_,  nullptr,	// (24) タイマＲＢ２
+		(void*)null_task_,  nullptr,	// (25) /INT1
+		(void*)null_task_,  nullptr,	// (26) /INT3
+		(void*)null_task_,  nullptr,	// (27)
+
+		(void*)null_task_,  nullptr,	// (28)
+		(void*)null_task_,  nullptr,	// (29) /INT0
+		(void*)null_task_,  nullptr,	// (30)
+		(void*)null_task_,  nullptr,	// (31)
+	};
+}
+
+
+int main(int argc, char *argv[])
+{
+	using namespace device;
+
+// クロック関係レジスタ・プロテクト解除
+	PRCR.PRC0 = 1;
+
+// 高速オンチップオシレーターへ切り替え
+	OCOCR.HOCOE = 1;
+	wait_(1000);
+	SCKCR.HSCKSEL = 1;
+	CKSTPR.SCKSEL = 1;
+
+// L チカ・メイン
+	PD1.B0 = 1;
+	while(1) {
+		P1.B0 = 0;
+		for(uint32_t i = 0; i < 300000; ++i) { asm("nop"); }
+		P1.B0 = 1;
+		for(uint32_t i = 0; i < 300000; ++i) { asm("nop"); }
+	}
+
+}
