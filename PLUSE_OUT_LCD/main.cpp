ebaa03c41e2f904afc4f0cdefddd648aba1dd7b3
hirakuni45 <hira@rvf-rc45.net>
2015-07-24 00:43:23 +0900

    Update

--- PLUSE_OUT_LCD/main.cpp
+++ PLUSE_OUT_LCD/main.cpp
@@ -1,21 +1,79 @@
 //=====================================================================//
 /*!	@file
-	@brief	R8C タイマーJ、パルス出力 メイン
+	@brief	R8C PLUSE OUT/LCD メイン @n
+			for ST7567 SPI (128 x 32) @n
+			LCD: Aitendo M-G0812P7567
 	@author	平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
-#include "main.hpp"
 #include <cstring>
+#include "main.hpp"
 #include "system.hpp"
 #include "clock.hpp"
-#include "port.hpp"
 #include "common/command.hpp"
-#include "common/delay.hpp"
-#include "common/port_map.hpp"
+#include "common/format.hpp"
+#include "common/monograph.hpp"
+#include "port_def.hpp"
 
-static timer_b timer_b_;
+#include "bitmap/font32.h"
+
+static const uint8_t* nmbs_[] = {
+	nmb_0, nmb_1, nmb_2, nmb_3, nmb_4,
+	nmb_5, nmb_6, nmb_7, nmb_8, nmb_9,
+	txt_hz, txt_k
+};
+
+static uint8_t enc_lvl_ = 0;
+static volatile int8_t enc_cnt_ = 0;
+
+class encoder {
+public:
+	void operator() () {
+		uint8_t lvl = device::P1();  ///< 状態の取得
+		uint8_t pos = ~enc_lvl_ &  lvl;  ///< 立ち上がりエッジ検出
+		uint8_t neg =  enc_lvl_ & ~lvl;  ///< 立ち下がりエッジ検出
+		enc_lvl_ = lvl;  ///< 状態のセーブ
+
+		if(pos & device::P1.B0.b()) {
+			if(lvl & device::P1.B1.b()) {
+				--enc_cnt_;
+			} else {
+				++enc_cnt_;
+			}
+		}
+		if(neg & device::P1.B0.b()) {
+			if(lvl & device::P1.B1.b()) {
+				++enc_cnt_;
+			} else {
+				--enc_cnt_;
+			}
+		}
+		if(pos & device::P1.B1.b()) {
+			if(lvl & device::P1.B0.b()) {
+				++enc_cnt_;
+			} else {
+				--enc_cnt_;
+			}
+		}
+		if(neg & device::P1.B1.b()) {
+			if(lvl & device::P1.B0.b()) {
+				--enc_cnt_;
+			} else {
+				++enc_cnt_;
+			}
+		}
+	}
+};
+
+
+static device::trb_io<encoder> timer_b_;
 static uart0 uart0_;
 static utils::command<64> command_;
+static spi_base spi_base_;
+static spi_ctrl spi_ctrl_;
+static lcd lcd_;
+static graphics::monograph bitmap_;
+static timer_j timer_j_;
 
 extern "C" {
 	void sci_putch(char ch) {
@@ -34,8 +92,6 @@ extern "C" {
 		uart0_.puts(str);
 	}
 }
-
-static timer_j timer_j_;
 
 extern "C" {
 	const void* variable_vectors_[] __attribute__ ((section (".vvec"))) = {
@@ -82,67 +138,7 @@ extern "C" {
 }
 
 
-static bool check_key_word_(uint8_t idx, const char* key)
-{
-	char buff[12];
-	if(command_.get_word(idx, sizeof(buff), buff)) {
-		if(strcmp(buff, key) == 0) {
-			return true;
-		}				
-	}
-	return false;
-}
-
-
-static bool get_dec_(const char* text, uint32_t& val) {
-	val = 0;
-	char ch;
-	while((ch = *text++) != 0) {
-		if(ch >= '0' && ch <= '9') {
-			ch -= '0';
-		} else {
-			return false;
-		}
-		val *= 10;
-		val += ch;
-	}
-	return true;
-}
-
-
-static bool get_decimal_(uint8_t no, uint32_t& val) {
-	char buff[9];
-	if(command_.get_word(no, sizeof(buff), buff)) {
-		if(get_dec_(buff, val)) {
-			return true;
-		}
-	}
-	return false;
-}
-
-
-static bool help_(uint8_t cmdn) {
-	if(cmdn >= 1 && check_key_word_(0, "help")) {
-		sci_puts("freq FREQUENCY[Hz]\n");
-		return true;
-	}
-	return false;
-}
-
-
-static bool freq_(uint8_t cmdn) {
-	if(cmdn >= 2) {
-		uint32_t val;
-		if(get_decimal_(1, val)) {
-			if(!timer_j_.set_cycle(val)) {
-				sci_puts("TRJ out of range.\n");
-			}
-			return true;
-		}
-	}
-	return false;
-}
-
+__attribute__ ((section (".exttext")))
 int main(int argc, char *argv[])
 {
 	using namespace device;
@@ -157,19 +153,45 @@ int main(int argc, char *argv[])
 	SCKCR.HSCKSEL = 1;
 	CKSTPR.SCKSEL = 1;
 
+	PRCR.PRC0 = 0;
+
 	// タイマーＢ初期化
 	{
 		uint8_t ir_level = 2;
-		timer_b_.start_timer(60, ir_level);
+		timer_b_.start_timer(240, ir_level);
 	}
 
-	// UART の設定 (P1_4: TXD0[in], P1_5: RXD0[in])
+	// UART の設定 (P1_4: TXD0[out], P1_5: RXD0[in])
 	// ※シリアルライターでは、RXD 端子は、P1_6 となっているので注意！
 	{
 		utils::PORT_MAP(utils::port_map::P14::TXD0);
 		utils::PORT_MAP(utils::port_map::P15::RXD0);
 		uint8_t ir_level = 1;
 		uart0_.start(19200, ir_level);
+	}
+
+	// エンコーダー入力の設定 P10: (Phi_A), P11: (Phi_B), Vss: (COM)
+	{
+		utils::PORT_MAP(utils::port_map::P10::PORT);
+		utils::PORT_MAP(utils::port_map::P11::PORT);
+		device::PD1.B0 = 0;
+		device::PD1.B1 = 0;
+		device::PUR1.B0 = 1;	///< プルアップ
+		device::PUR1.B1 = 1;	///< プルアップ
+	}
+
+	// spi_base, spi_ctrl ポートの初期化
+	{
+		spi_ctrl_.init();
+		spi_base_.init();
+	}
+
+	// LCD を開始
+	{
+		lcd_.start();
+		bitmap_.init();
+		bitmap_.clear(0);
+//		bitmap_.frame(0, 0, 128, 32, 1);
 	}
 
 	// TRJ のパルス出力設定
@@ -180,24 +202,71 @@ int main(int argc, char *argv[])
 		}
 	}
 
-	sci_puts("Start R8C PLUSE output monitor\n");
-
+	sci_puts("Start R8C PLUSE OUT/LCD\n");
 	command_.set_prompt("# ");
 
+	uint8_t cnt = 0;
+	uint32_t count = 0;
 	while(1) {
 		timer_b_.sync();
 
-		// コマンド入力と、コマンド解析
-		if(command_.service()) {
-			uint8_t cmdn = command_.get_words();
-			if(cmdn == 0) ;
-			else if(help_(cmdn)) ;
-			else if(freq_(cmdn)) ;
-			else {
-				sci_puts("Command error: ");
-				sci_puts(command_.get_command());
-				sci_putch('\n');
-			}
+		// エンコーダー値の増減
+		int32_t d = 0;
+		if(enc_cnt_ >= 4) {
+			enc_cnt_ = 0;
+			d = 1;
+		} else if(enc_cnt_ <= -4) { 
+			enc_cnt_ = 0;
+			d = -1;
 		}
+		if(d) {
+			if(count < 100) {
+				d *= 1;
+			} else if(count < 1000) { // 1KHz
+				d *= 10; // 10Hz step
+			} else if(count < 10000) { // 10KHz
+				d *= 100; // 100Hz step
+			} else if(count < 100000) { // 100KHz
+				d *= 1000; // 1KHz step
+			} else if(count < 1000000) { // 1MHz
+				d *= 10000; // 10KHz step
+			} else {
+				d *= 100000; // 100KHz step
+			}
+			count += static_cast<uint32_t>(d);
+			if(count < 20) count = 20;
+			else if(count > 10000000) count = 10000000;
+
+			timer_j_.set_cycle(count);
+		}
+
+		// 15Hz
+		if((cnt & 15) == 0) {
+			bitmap_.clear(0);
+			uint32_t n = count;
+			bool khz = false;
+			if(n > 99999) {
+				n /= 1000;
+				khz = true;
+			}
+			bitmap_.draw_mobj(20 * 4, 0, nmbs_[n % 10]);
+			n /= 10;
+			bitmap_.draw_mobj(20 * 3, 0, nmbs_[n % 10]);
+			n /= 10;
+			bitmap_.draw_mobj(20 * 2, 0, nmbs_[n % 10]);
+			n /= 10;
+			bitmap_.draw_mobj(20 * 1, 0, nmbs_[n % 10]);
+			n /= 10;
+			bitmap_.draw_mobj(20 * 0, 0, nmbs_[n % 10]);
+			if(khz) {
+				bitmap_.draw_mobj(20 * 5, 0, nmbs_[11]);
+				bitmap_.draw_mobj(20 * 5 + 11, 0, nmbs_[10]);
+			} else {
+				bitmap_.draw_mobj(20 * 5, 0, nmbs_[10]);
+			}
+			lcd_.copy(bitmap_.fb());
+		}
+
+		++cnt;
 	}
 }
