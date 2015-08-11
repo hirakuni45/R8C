0d3f3e213ffde9c3c7183455390acb9a7d9ac94f
hirakuni45 <hira@rvf-rc45.net>
2015-08-11 18:47:06 +0900

    Add header file, change function name

--- common/init.c
+++ common/init.c
@@ -1,9 +1,12 @@
 //=====================================================================//
 /*! @file
-    @brief  R8C 起動前、初期化
+    @brief  R8C 起動前初期化
     @author 平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
+#include <stdlib.h>
+#include "common/init.h"
+
 int main(int argc, char**argv);
 
 extern short _datainternal;
@@ -18,10 +21,14 @@ extern short _preinit_array_end;
 extern short _init_array_start;
 extern short _init_array_end;
 
-int init(void)
+//-----------------------------------------------------------------//
+/*!
+	@brief  メイン関数起動前初期化
+*/
+//-----------------------------------------------------------------//
+void _init(void)
 {
-	// R/W-data セクションのコピー
-	{
+	{  // R/W-data セクションのコピー
 		short *src = &_datainternal;
 		short *dst = &_datastart;
 		while(dst < &_dataend) {
@@ -29,23 +36,22 @@ int init(void)
 		}
 	}
 
-	// bss セクションのクリア
-	{
+	{  // bss セクションのクリア
 		short *dst = &_bssstart;
 		while(dst < &_bssend) {
 			*dst++ = 0;
 		}
 	}
 
-	// 静的コンストラクターの実行（C++ ）
-	{
+	{  // C++ 事前静的コンストラクターの実行
 		short *p = &_preinit_array_start;
 		while(p < &_preinit_array_end) {
 			void (*prog)(void) = (void *)*p++;
 			(*prog)();
 		}
 	}
-	{
+
+	{  // C++ 静的コンストラクターの実行
 		short *p = &_init_array_start;
 		while(p < &_init_array_end) {
 			void (*prog)(void) = (void *)*p++;
@@ -57,12 +63,7 @@ int init(void)
 	static int argc = 0;
 	static char **argv = 0;
 	int ret = main(argc, argv);
-
-	return ret;
-}
-
-void __dso_handle(void)
-{
+	exit(0);
 }
 
 // EOF
