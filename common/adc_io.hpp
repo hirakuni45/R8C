dc9d80ef93fdb8b7d050513094e070c1169906c8
hirakuni45 <hira@rvf-rc45.net>
2015-11-15 01:33:44 +0900

    設定の明確化

--- common/adc_io.hpp
+++ common/adc_io.hpp
@@ -21,10 +21,16 @@ namespace device {
 	class adc_io {
 
 	public:
-		enum class cnv_type {
-			chanel0,	//< チャネル０のみ
-			chanel1,	//< チャネル１のみ
-			multi		//< ０，１チャネル
+		enum class cnv_type : uint8_t {
+			CH0,		//< チャネル０のみ
+			CH1,		//< チャネル１のみ
+			CH0_CH1		//< ０，１チャネル
+		};
+
+		enum class ch_grp : uint8_t {
+			AN0_AN1,	//< AN0, AN1
+			AN2_AN3,	//< AN2, AN3
+			AN4_AN7		//< AN4, AN7
 		};
 
 	private:
@@ -45,23 +51,25 @@ namespace device {
 		//-----------------------------------------------------------------//
 		/*!
 			@brief  変換開始
-			@param[in]	cycle	繰り返しの場合「true」
+			@param[in]	ct		変換タイプ
+			@param[in]	grp		グループ
+			@param[in]	cycle	繰り返し変換の場合「true」
 			@param[in]	ir_lvl	割り込みレベル（０の場合割り込みを使用しない）
 		*/
 		//-----------------------------------------------------------------//
-		void setup(bool cycle, cnv_type ct, uint8_t grp, uint8_t ir_lvl = 0) {
+		void setup(cnv_type ct, ch_grp cg, bool cycle, uint8_t ir_lvl = 0) {
 			ADCON0.ADST = 0;
 
 			MSTCR.MSTAD = 0;
 
 			uint8_t md = 0;
-			if(ct == cnv_type::multi) md = 2;
+			if(ct == cnv_type::CH0_CH1) md = 2;
 			if(cycle) ++md; 
 			ADMOD = ADMOD.CKS.b(3) | ADMOD.MD.b(md);
 
 			uint8_t chn = 0;
-			if(ct == cnv_type::chanel1) chn = 1; 
-			ADINSEL = ADINSEL.CH0.b(chn) | ADINSEL.ADGSEL.b(grp);
+			if(ct == cnv_type::CH1) chn = 1; 
+			ADINSEL = ADINSEL.CH0.b(chn) | ADINSEL.ADGSEL.b(static_cast<uint8_t>(cg));
 		}
 
 
