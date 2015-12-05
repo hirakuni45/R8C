825ad8a189898175e97215da273fa84920551d00
平松邦仁 <hirakuni45@URANUSMacBook-Pro.local>
2015-12-05 17:33:51 +0900

    all make shell

---
+++ all_make.sh
@@ -0,0 +1,60 @@
+cd ADC_test
+make
+cd ..
+cd COMP_test
+make
+cd ..
+cd ENCODER_test
+make
+cd ..
+cd FLASH_test
+make
+cd ..
+cd I2C_EEPROM_test
+make
+cd ..
+cd I2C_RTC_test
+make
+cd ..
+cd LCD_test
+make
+cd ..
+cd LED_test
+make
+cd ..
+cd L_chika
+make
+cd ..
+cd PLUSE_INP_test
+make
+cd ..
+cd PLUSE_OUT_LCD
+make
+cd ..
+cd PLUSE_OUT_test
+make
+cd ..
+cd PWM_test
+make
+cd ..
+cd RC_SERVO_test
+make
+cd ..
+cd SD_WAV_play
+make
+cd ..
+cd SD_monitor
+make
+cd ..
+cd SD_test
+make
+cd ..
+cd SWITCH_test
+make
+cd ..
+cd TIMER_test
+make
+cd ..
+cd UART_test
+make
+cd ..
