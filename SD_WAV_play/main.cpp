1ad77236359d1e442ed4728e173da1f5a6456f2f
hira <hira@rvf-rc45.net>
2016-09-25 17:16:03 +0900

    update template I/O

--- SD_WAV_play/main.cpp
+++ SD_WAV_play/main.cpp
@@ -4,68 +4,88 @@
 	@author	平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
-#include "main.hpp"
+#include <cstring>
 #include "system.hpp"
 #include "clock.hpp"
+#include "common/intr_utils.hpp"
 #include "common/delay.hpp"
 #include "common/port_map.hpp"
+#include "common/uart_io.hpp"
+#include "common/fifo.hpp"
 #include "common/command.hpp"
-#include <cstring>
 #include "common/format.hpp"
+#include "common/trb_io.hpp"
+#include "common/trc_io.hpp"
+
 #include "pfatfs/src/pff.h"
+
+#include "port_def.hpp"
+
 #include "wav_in.hpp"
 
-static timer_c timer_c_;
+namespace {
 
-struct wave_t {
-	uint8_t	left;
-	uint8_t	right;
-};
+	typedef device::trb_io<utils::null_task> timer_b;
+	timer_b timer_b_;
 
-static wave_t wave_buff_[256];
-static volatile uint8_t wave_put_ = 0;
-static volatile uint8_t wave_get_ = 0;
+	typedef device::trc_io<utils::null_task> timer_c;
+	timer_c timer_c_;
 
-static void clear_wave_() {
-	for(uint16_t i = 0; i < 256; ++i) {
-		wave_buff_[i].left  = 128;
-		wave_buff_[i].right = 128;
+	struct wave_t {
+		uint8_t	left;
+		uint8_t	right;
+	};
+
+	wave_t wave_buff_[256];
+	volatile uint8_t wave_put_ = 0;
+	volatile uint8_t wave_get_ = 0;
+
+	void clear_wave_() {
+		for(uint16_t i = 0; i < 256; ++i) {
+			wave_buff_[i].left  = 128;
+			wave_buff_[i].right = 128;
+		}
 	}
+
+	class wave_out {
+		public:
+		void operator() () {
+			const volatile wave_t& t = wave_buff_[wave_get_];
+			timer_c_.set_pwm_b(t.left);
+			timer_c_.set_pwm_c(t.right);
+			++wave_get_;
+		}
+	};
+
+	typedef device::trb_io<wave_out> timer_audio;
+
+	audio::wav_in wav_in_;
+
+	typedef utils::fifo<uint8_t, 16> buffer;
+	typedef device::uart_io<device::UART0, buffer, buffer> uart;
+	uart uart_;
+
+	utils::command<64> command_;
+
+	spi_base spi_base_;
+	spi_ctrl spi_ctrl_;
 }
-
-class wave_out {
-	public:
-	void operator() () {
-		const volatile wave_t& t = wave_buff_[wave_get_];
-		timer_c_.set_pwm_b(t.left);
-		timer_c_.set_pwm_c(t.right);
-		++wave_get_;
-	}
-};
-
-typedef device::trb_io<wave_out> timer_audio;
-static uart0 uart0_;
-static spi_base spi_base_;
-static spi_ctrl spi_ctrl_;
-static timer_audio timer_b_;
-
-static audio::wav_in wav_in_;
 
 extern "C" {
 	void sci_putch(char ch) {
-		uart0_.putch(ch);
+		uart_.putch(ch);
 	}
 
 	char sci_getch(void) {
-		return uart0_.getch();
+		return uart_.getch();
 	}
 
 	uint16_t sci_length() {
-		return uart0_.length();
+		return uart_.length();
 	}
 
 	void sci_puts(const char* str) {
-		uart0_.puts(str);
+		uart_.puts(str);
 	}
 }
 
@@ -92,8 +112,8 @@ extern "C" {
 		reinterpret_cast<void*>(null_task_),	nullptr,	// (15)
 
 		reinterpret_cast<void*>(null_task_),	nullptr,	// (16)
-		reinterpret_cast<void*>(uart0_.isend),	nullptr,	// (17) UART0 送信
-		reinterpret_cast<void*>(uart0_.irecv),	nullptr,	// (18) UART0 受信
+		reinterpret_cast<void*>(uart_.isend),	nullptr,	// (17) UART0 送信
+		reinterpret_cast<void*>(uart_.irecv),	nullptr,	// (18) UART0 受信
 		reinterpret_cast<void*>(null_task_),	nullptr,	// (19)
 
 		reinterpret_cast<void*>(null_task_),	nullptr,	// (20)
@@ -183,7 +203,7 @@ int main(int argc, char *argv[])
 		utils::PORT_MAP(utils::port_map::P14::TXD0);
 		utils::PORT_MAP(utils::port_map::P15::RXD0);
 		uint8_t intr_level = 1;
-		uart0_.start(19200, intr_level);
+		uart_.start(19200, intr_level);
 	}
 
 	// spi_base, spi_ctrl ポートの初期化
