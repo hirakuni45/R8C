55c840d2e5d9ab0c31a322282e43997f9860667f
hira <hira@rvf-rc45.net>
2016-11-15 23:25:16 +0900

    update port class

--- common/spi_io.hpp
+++ common/spi_io.hpp
@@ -1,32 +1,77 @@
 #pragma once
 //=====================================================================//
 /*!	@file
-	@brief	SPI テンプレートクラス (20MHz system clock) @n
-			Copyright 2015 Kunihito Hiramatsu
+	@brief	ソフトウェア SPI テンプレートクラス @n
+			Copyright 2016 Kunihito Hiramatsu
 	@author	平松邦仁 (hira@rvf-rc45.net)
 */
 //=====================================================================//
 #include <cstdint>
-#include "common/delay.hpp"
 
 namespace device {
 
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
 	/*!
-		@brief  SPI テンプレートクラス @n
-				PORT ポート指定クラス： @n
-				class port { @n
-				public: @n
-					void scl_out(bool val) const { } @n
-					void sda_out(bool val) const { } @n
-					bool sda_inp() const { return b; } @n
-				};
-		@param[in]	PORT	ポート定義クラス
+		@brief  ソフトウェア SPI テンプレートクラス
+		@param[in]	CLK	クロック・クラス
+		@param[in]	OUT	クロック・クラス
+		@param[in]	INP	クロック・クラス
 	*/
 	//+++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++++//
-	template <class PORT>
+	template <class CLK, class OUT, class INP>
 	class spi_io {
-		PORT		port_;
+
+		uint8_t	delay_;
+
+		inline void clock_() {
+			uint8_t n = delay_;
+			while(n > 3) { --n; asm("nop"); }
+			CLK::P = 1;
+			n = delay_;
+			while(n > 0) { --n; asm("nop"); }
+			CLK::P = 0;
+		} 
+
+		void send_(uint8_t d) {
+			if (d & 0x80) OUT::P = 1; else OUT::P = 0;	/* bit7 */
+			clock_();
+			if (d & 0x40) OUT::P = 1; else OUT::P = 0;	/* bit6 */
+			clock_();
+			if (d & 0x20) OUT::P = 1; else OUT::P = 0;	/* bit5 */
+			clock_();
+			if (d & 0x10) OUT::P = 1; else OUT::P = 0;	/* bit4 */
+			clock_();
+			if (d & 0x08) OUT::P = 1; else OUT::P = 0;	/* bit3 */
+			clock_();
+			if (d & 0x04) OUT::P = 1; else OUT::P = 0;	/* bit2 */
+			clock_();
+			if (d & 0x02) OUT::P = 1; else OUT::P = 0;	/* bit1 */
+			clock_();
+			if (d & 0x01) OUT::P = 1; else OUT::P = 0;	/* bit0 */
+			clock_();
+		}
+
+		uint8_t recv_(bool out = 1) {
+			OUT::P = out;
+			uint8_t r = 0;
+			if(INP::P()) ++r;  // bit7
+			clock_();
+			r <<= 1; if(INP::P()) ++r;  // bit6
+			clock_();
+			r <<= 1; if(INP::P()) ++r;  // bit5
+			clock_();
+			r <<= 1; if(INP::P()) ++r;  // bit4
+			clock_();
+			r <<= 1; if(INP::P()) ++r;  // bit3
+			clock_();
+			r <<= 1; if(INP::P()) ++r;  // bit2
+			clock_();
+			r <<= 1; if(INP::P()) ++r;  // bit1
+			clock_();
+			r <<= 1; if(INP::P()) ++r;  // bit0
+			clock_();
+			return r;
+		}
 
 	public:
 		//-----------------------------------------------------------------//
@@ -34,68 +79,93 @@ namespace device {
 			@brief  コンストラクター
 		*/
 		//-----------------------------------------------------------------//
-		spi_io() { }
+		spi_io() : delay_(0) { }
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  初期化
+			@brief	開始
+			@param[in]	delay	クロック遅延（速度）
+			@return 成功なら「true」
 		*/
 		//-----------------------------------------------------------------//
-		void init() const {
-			port_.init();
-			port_.scl_out(1);
+		bool start(uint8_t delay = 0) {
+			delay_ = delay;
+			CLK::DIR = 1;
+			OUT::DIR = 1;
+			INP::DIR = 0;
+			return true;
+		}
+
+
+		//----------------------------------------------------------------//
+		/*!
+			@brief	リード・ライト
+			@param[in]	d	書き込みデータ
+			@return 読み出しデータ
+		*/
+		//----------------------------------------------------------------//
+		uint8_t xchg(uint8_t d = 0xff)
+		{
+			uint8_t r = 0;
+			if (d & 0x80) OUT::P = 1; else OUT::P = 0;	// bit7
+			if(INP::P()) ++r;  // bit7
+			clock_();
+			if (d & 0x40) OUT::P = 1; else OUT::P = 0;	// bit6
+			r <<= 1; if(INP::P()) ++r;  // bit6
+			clock_();
+			if (d & 0x20) OUT::P = 1; else OUT::P = 0;	// bit5
+			r <<= 1; if(INP::P()) ++r;  // bit5
+			clock_();
+			if (d & 0x10) OUT::P = 1; else OUT::P = 0;	// bit4
+			r <<= 1; if(INP::P()) ++r;  // bit4
+			clock_();
+			if (d & 0x08) OUT::P = 1; else OUT::P = 0;	// bit3
+			r <<= 1; if(INP::P()) ++r;  // bit3
+			clock_();
+			if (d & 0x04) OUT::P = 1; else OUT::P = 0;	// bit2
+			r <<= 1; if(INP::P()) ++r;  // bit2
+			clock_();
+			if (d & 0x02) OUT::P = 1; else OUT::P = 0;	// bit1
+			r <<= 1; if(INP::P()) ++r;  // bit1
+			clock_();
+			if (d & 0x01) OUT::P = 1; else OUT::P = 0;	// bit0
+			r <<= 1; if(INP::P()) ++r;  // bit0
+			clock_();
+			return r;
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  ライト
-			@param[in]	d	データ
+			@brief  シリアル送信
+			@param[in]	src		送信ソース
+			@param[in]	size	送信サイズ
 		*/
 		//-----------------------------------------------------------------//
-		void write(uint8_t d) const {
-			port_.scl_out(0);
-			port_.sda_out((d >> 7) & 1);
-			port_.scl_out(1);
-			port_.scl_out(0);
-			port_.sda_out((d >> 6) & 1);
-			port_.scl_out(1);
-			port_.scl_out(0);
-			port_.sda_out((d >> 5) & 1);
-			port_.scl_out(1);
-			port_.scl_out(0);
-			port_.sda_out((d >> 4) & 1);
-			port_.scl_out(1);
-			port_.scl_out(0);
-			port_.sda_out((d >> 3) & 1);
-			port_.scl_out(1);
-			port_.scl_out(0);
-			port_.sda_out((d >> 2) & 1);
-			port_.scl_out(1);
-			port_.scl_out(0);
-			port_.sda_out((d >> 1) & 1);
-			port_.scl_out(1);
-			port_.scl_out(0);
-			port_.sda_out(d & 1);
-			port_.scl_out(1);
+		void send(const uint8_t* src, uint16_t size)
+		{
+			auto end = src + size;
+			while(src < end) {
+				send_(*src);
+				++src;
+			}
 		}
 
 
 		//-----------------------------------------------------------------//
 		/*!
-			@brief  ライト
-			@param[in]	d	データ
+			@brief  シリアル受信
+			@param[out]	dst	受信先
+			@param[in]	cnt	受信サイズ
 		*/
 		//-----------------------------------------------------------------//
-		void write_1(uint8_t d) const {
-			for(uint8_t i = 0; i < 8; ++i) {
-				port_.scl_out(0);
-				port_.sda_out(d & 0x80);
-				asm("nop");
-				port_.scl_out(1);
-				d <<= 1;
-				asm("nop");
+		void recv(uint8_t* dst, uint16_t size)
+		{
+			auto end = dst + size;
+			while(dst < end) {
+				*dst = recv_();
+				++dst;
 			}
 		}
 
