e6c0223723d28cdb2208e2a6ff6d7528bdbd4160
hira <hira@rvf-rc45.net>
2016-11-18 22:30:15 +0900

    update page offset

--- chip/ST7565.hpp
+++ chip/ST7565.hpp
@@ -181,16 +181,17 @@ namespace chip {
 			@brief  コピー
 			@param[in]	src	フレームバッファソース
 			@param[in]	num	転送ページ数
+			@param[in]	ofs	転送オフセット
 		*/
 		//-----------------------------------------------------------------//
-		void copy(const uint8_t* src, uint8_t num) {
+		void copy(const uint8_t* src, uint8_t num, uint8_t ofs = 0) {
 			chip_enable_();
-			uint8_t ofs = 0x00;
-			for(uint8_t page = 0; page < num; ++page) {
+			uint8_t o = 0x00;
+			for(uint8_t page = ofs; page < (ofs + num); ++page) {
 				reg_select_(0);
 				write_(CMD::SET_PAGE, page);
-				write_(CMD::SET_COLUMN_LOWER, ofs & 0x0f);
-				write_(CMD::SET_COLUMN_UPPER, ofs >> 4);
+				write_(CMD::SET_COLUMN_LOWER, o & 0x0f);
+				write_(CMD::SET_COLUMN_UPPER, o >> 4);
     			write_(CMD::RMW);
 				reg_select_(1);
 				csi_.send(src, 128);
