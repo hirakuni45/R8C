0386b416267e081ed5410a3efbb918701c0eb397
hira <hira@rvf-rc45.net>
2017-05-11 15:20:34 +0900

    new project

---
+++ MAX6675_sample/main.cpp
@@ -0,0 +1,150 @@
+//=====================================================================//
+/*!	@file
+	@brief	R8C MAX6675・メイン
+			Copyright 2017 Kunihito Hiramatsu
+	@author	平松邦仁 (hira@rvf-rc45.net)
+*/
+//=====================================================================//
+#include "system.hpp"
+#include "clock.hpp"
+#include "port.hpp"
+#include "common/delay.hpp"
+#include "common/intr_utils.hpp"
+#include "common/port_map.hpp"
+#include "common/format.hpp"
+#include "common/fifo.hpp"
+#include "common/uart_io.hpp"
+#include "common/adc_io.hpp"
+#include "common/trb_io.hpp"
+#include "chip/MAX6675.hpp"
+
+namespace {
+
+	typedef device::trb_io<utils::null_task, uint8_t> timer_b;
+	timer_b timer_b_;
+
+	typedef utils::fifo<uint8_t, 16> buffer;
+	typedef device::uart_io<device::UART0, buffer, buffer> uart;
+	uart uart_;
+
+	typedef device::adc_io<utils::null_task> adc;
+	adc adc_;
+
+
+}
+
+extern "C" {
+	void sci_putch(char ch) {
+		uart_.putch(ch);
+	}
+
+	char sci_getch(void) {
+		return uart_.getch();
+	}
+
+	uint16_t sci_length() {
+		return uart_.length();
+	}
+
+	void sci_puts(const char* str) {
+		uart_.puts(str);
+	}
+}
+
+extern "C" {
+	const void* variable_vectors_[] __attribute__ ((section (".vvec"))) = {
+		reinterpret_cast<void*>(brk_inst_),		nullptr,	// (0)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (1) flash_ready
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (2)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (3)
+
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (4) コンパレーターB1
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (5) コンパレーターB3
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (6)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (7) タイマＲＣ
+
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (8)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (9)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (10)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (11)
+
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (12)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (13) キー入力
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (14) A/D 変換
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (15)
+
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (16)
+		reinterpret_cast<void*>(uart_.isend),	nullptr,	// (17) UART0 送信
+		reinterpret_cast<void*>(uart_.irecv),	nullptr,	// (18) UART0 受信
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (19)
+
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (20)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (21) /INT2
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (22) タイマＲＪ２
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (23) 周期タイマ
+
+		reinterpret_cast<void*>(timer_b_.itask),nullptr,	// (24) タイマＲＢ２
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (25) /INT1
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (26) /INT3
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (27)
+
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (28)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (29) /INT0
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (30)
+		reinterpret_cast<void*>(null_task_),	nullptr,	// (31)
+	};
+}
+
+// __attribute__ ((section (".exttext")))
+int main(int argc, char *argv[])
+{
+	using namespace device;
+
+// クロック関係レジスタ・プロテクト解除
+	PRCR.PRC0 = 1;
+
+// 高速オンチップオシレーターへ切り替え(20MHz)
+// ※ F_CLK を設定する事（Makefile内）
+	OCOCR.HOCOE = 1;
+	utils::delay::micro_second(1);  // >=30us(125KHz)
+	SCKCR.HSCKSEL = 1;
+	CKSTPR.SCKSEL = 1;
+
+	// タイマーＢ初期化
+	{
+		uint8_t ir_level = 2;
+		timer_b_.start_timer(60, ir_level);
+	}
+
+	// UART の設定 (P1_4: TXD0[out], P1_5: RXD0[in])
+	// ※シリアルライターでは、RXD 端子は、P1_6 となっているので注意！
+	{
+		utils::PORT_MAP(utils::port_map::P14::TXD0);
+		utils::PORT_MAP(utils::port_map::P15::RXD0);
+		uint8_t intr_level = 1;
+		uart_.start(57600, intr_level);
+	}
+
+	uart_.puts("Start R8C MAX6675 sample\n");
+
+
+	using namespace utils;
+
+	uint8_t cnt = 0;
+	while(1) {
+		timer_b_.sync();
+
+		++cnt;
+		if(cnt >= 30) {
+			cnt = 0;
+
+
+
+		}
+
+		if(uart_.length()) {  // UART のレシーブデータがあるか？
+			auto ch = uart_.getch();
+			uart_.putch(ch);
+		}
+	}
+}
